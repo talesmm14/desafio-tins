ca9865906263de175d26ddbf5caafc9dcf0be68d
tales.melquiades <tales.melquiades@secad.to.gov.br>
2023-04-24 09:28:31 -0300

    Configurações de email

--- apps/loja/signals/endereco.py
+++ apps/loja/signals/endereco.py
@@ -1,11 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from apps.loja.models import Endereco
+from apps.loja.models import ClienteEndereco
 
 
-@receiver(post_save, sender=Endereco)
+@receiver(post_save, sender=ClienteEndereco)
 def set_endereco_padrao(sender, instance, created, **kwargs):
     # Atualiza todos os outros endereços para padrão False caso esse seja o endereço marcado como padrão.
-    if instance.padrao:
-        Endereco.objects.exclude(id=instance.id).update(padrao=False)
+    if instance.default:
+        ClienteEndereco.objects.exclude(id=instance.id).update(default=False)
